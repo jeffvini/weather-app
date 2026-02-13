79e8da718ce2a3ea41e04f6e44179e4f4f70efed
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-02-13 18:47:08 -0300

    refactor(WeatherCard): improve gap and refine card shadow for better depth

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -6,7 +6,7 @@ export function WeatherCard() {
   return (
     <article
       aria-labelledby="city-name"
-      className="flex w-full flex-col rounded-2xl bg-white p-6 shadow-[0_15px_10px_-10px_rgba(0,0,0,0.3)] md:w-auto"
+      className="flex w-full flex-col gap-6 rounded-2xl bg-white p-6 shadow-[0_0_7px_3px_rgba(0,0,0,0.2)] md:w-auto"
     >
       <SearchBar />
       <WeatherMain />
