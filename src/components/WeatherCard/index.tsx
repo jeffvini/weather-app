ff4d917cf9e5d9fb7dcaa2592c39a7667602e112
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-02-13 16:34:11 -0300

    refactor(WeatherCard): decouple layout margins and increase internal padding

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -6,7 +6,7 @@ export function WeatherCard() {
   return (
     <article
       aria-labelledby="city-name"
-      className="mx-4 flex w-full flex-col rounded-2xl bg-white shadow-[0_15px_10px_-10px_rgba(0,0,0,0.3)] md:w-auto"
+      className="flex w-full flex-col rounded-2xl bg-white p-6 shadow-[0_15px_10px_-10px_rgba(0,0,0,0.3)] md:w-auto"
     >
       <SearchBar />
       <WeatherMain />
