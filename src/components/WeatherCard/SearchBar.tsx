a1b6b23374a406dfaf9b5da7fbae26d1a81a7208
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-02-14 17:49:11 -0300

    feat: implement loading state with dynamic UI and a11y feedback

--- src/components/WeatherCard/SearchBar.tsx
+++ src/components/WeatherCard/SearchBar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { MapPin } from 'lucide-react';
 import { X } from 'lucide-react';
 import { Search } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
 
 interface SearchBarProps {
   onSubmit: (cityName: string) => void;
@@ -26,6 +27,7 @@ export function SearchBar({ onSubmit, isLoading }: SearchBarProps) {
       <MapPin className="fill-gray-700 text-white" />
       <input
         type="text"
+        disabled={isLoading}
         value={city}
         onChange={(e) => setCity(e.target.value)}
         aria-label="Search city"
@@ -42,8 +44,17 @@ export function SearchBar({ onSubmit, isLoading }: SearchBarProps) {
           <X className="text-gray-500" />
         </button>
       )}
-      <button type="submit" aria-label="Submit search" className="p-1">
-        <Search className="text-gray-500" />
+      <button
+        type="submit"
+        disabled={isLoading}
+        aria-label={!isLoading ? 'Submit search' : 'Searching...'}
+        className="p-1 disabled:cursor-not-allowed"
+      >
+        {!isLoading ? (
+          <Search className="text-gray-500" />
+        ) : (
+          <Loader2 className="animate-spin text-gray-500" />
+        )}
       </button>
     </form>
   );
