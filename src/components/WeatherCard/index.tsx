9c232f75b552f3105a8ceb2a1ba86d4e27f53dde
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-02-15 20:44:39 -0300

    feat(WeatherCard): integrate useWeather hook and conditional rendering logic

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,38 +1,29 @@
-import { useState, useEffect } from 'react';
-
 import { SearchBar } from './SearchBar';
 import { WeatherMain } from './WeatherMain';
 import { WeatherInfo } from './WeatherInfo';
 
-interface WeatherData {
-  city: string;
-  countryCode: string;
-  weatherCode: number;
-  temp: number;
-  tempMax: number;
-  tempMin: number;
-  humidity: number;
-  windSpeed: number;
-}
+import { useWeather } from '../../custom-hooks/useWeather';
 
 export function WeatherCard() {
-  const [cityQuery, setCityQuery] = useState<string>('Rio de Janeiro');
-  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const handleReceive = (city: string): void => {
-    setCityQuery(city);
-    setIsLoading(true);
-  };
-
+  const { weatherData, isLoading, error, handleReceive } = useWeather();
   return (
     <article
       aria-labelledby="city-name"
       className="flex w-full flex-col gap-6 rounded-2xl bg-white p-6 shadow-[0_0_7px_3px_rgba(0,0,0,0.2)] md:w-auto"
     >
       <SearchBar onSubmit={handleReceive} isLoading={isLoading} />
-      <WeatherMain />
-      <WeatherInfo />
+      {error && (
+        <p className="text-center text-2xl font-medium text-red-500">{error}</p>
+      )}
+      {isLoading && (
+        <p className="text-center text-2xl font-medium">Loading...</p>
+      )}
+      {weatherData && !isLoading && (
+        <>
+          <WeatherMain weatherData={weatherData} />
+          <WeatherInfo weatherData={weatherData} />
+        </>
+      )}
     </article>
   );
 }
